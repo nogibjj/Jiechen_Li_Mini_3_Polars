16cc813e9269443471f3ecb8ce5ad6d7306da1c3
Jiechen <119739006+carrieli15@users.noreply.github.com>
2023-09-16 03:04:46 +0000

    first commit

---
+++ spotify_main.py
@@ -0,0 +1,43 @@
+import polars as pl
+import matplotlib.pyplot as plt
+from pandas_profiling import ProfileReport
+  
+# read the CSV file 
+df = pl.read_csv('spotify_2023.csv', ignore_errors=True)
+
+
+def spotify_song():
+    return df
+
+# check the head
+# df.head()
+
+# compute summary statistics
+summary = df.describe()
+print(summary)
+
+track_col = ['streams','bpm', 'key', 'mode', 'danceability_%', 'valence_%', 'energy_%', 'acousticness_%', 'instrumentalness_%', 'liveness_%', 'speechiness_%']
+df_voi = df[track_col]
+df_voi.head()
+
+summary_col = df_voi.describe()
+print(summary_col)
+
+# Sort the DataFrame by 'popularity' in descending order
+df = pl.read_csv('spotify_2023.csv', ignore_errors=True)
+
+# Sort the DataFrame by 'popularity' in descending order
+df_sorted = df.sort("streams")
+
+# Select the top 10 songs
+top_songs = df_sorted.tail(10)
+print(top_songs)
+
+plt.figure(figsize=(12, 6))
+plt.barh(top_songs['track_name'], top_songs['streams'], color='skyblue')
+plt.xlabel('Popularity')
+plt.ylabel('Track Name')
+plt.title('Top 10 Songs with the Highest Popularity on Spotify')
+plt.gca().invert_yaxis()  # Invert the y-axis to display the most popular song at the top
+plt.xticks(rotation=0)  # Rotate x-axis labels if needed
+plt.show()
--- test_main.py
+++ test_main.py
@@ -3,22 +3,24 @@ Test goes here
 
 """
 
-from main import csvfile
+from spotify_main import spotify_song
 
 
-def test_csvfile():
-    main_data = "titanic_main_data.csv"
-    result = csvfile(main_data)
+def test_spotify_main():
+    main_data = "spotify_2023.csv"
+    result = df(main_data)
     
-    # mean value of the 1st column
-    mean_1_column = result.iloc[:, 0].mean()
-    # print(mean_1_column)
-    assert mean_1_column == 446
-    # min value of 1st column 
-    min_1_column = result.iloc[:, 0].min()
-    # print(min_1_column)
-    assert min_1_column == 1
+    print(result)
+
+    # mean value of the 3rd column
+    min_3_column = result.iloc[:, 4].min()
+    # print(mean_3_column)
+    assert min_3_column == 1.0
+    # std value of 3rd column 
+    std_3_column = result.iloc[:, 4].std()
+    # print(std_3_column)
+    assert std_3_column == 0.89
 
 
 if __name__ == "__main__":
-    test_csvfile()
+    test_spotify_main()
