02342e0aa84d4972a235edbb041097fcbf6ccff7
Jiechen <119739006+carrieli15@users.noreply.github.com>
2023-09-16 03:28:58 +0000

    final

--- test_main.py
+++ test_main.py
@@ -13,11 +13,12 @@ def test_spotify_main():
     print(result)
 
     # mean value of the 3rd column
-    min_3_column = result.iloc[:, 4].min()
+    # min_3_column = result.iloc[:, 4].min()
+    min_3_column = result.select_at_idx(4).agg(pl.col(0).min()).collect()[0][0]
     # print(mean_3_column)
     assert min_3_column == 1.0
     # std value of 3rd column 
-    std_3_column = result.iloc[:, 4].std()
+    std_3_column = result.select_at_idx(4).agg(pl.col(0).std()).collect()[0][0]
     # print(std_3_column)
     assert std_3_column == 0.89
 
